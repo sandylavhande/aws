28c622a819636467982b2e1f02ec9e24c1d6cc8c
root <root@ip-172-31-92-85.ec2.internal>
2023-11-27 07:20:58 +0000

    adding shell script

---
+++ minikube.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install curl
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube start --driver=none
+minikube status
